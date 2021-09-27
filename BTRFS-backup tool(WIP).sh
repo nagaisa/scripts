589d7bc2633dd7adf4782118dc96e6b328354fa6
nagaisa <82787802+nagaisa@users.noreply.github.com>
2021-09-27 12:59:08 -0400

    Create BTRFS-backup tool(WIP).sh

---
+++ BTRFS-backup tool(WIP).sh
@@ -0,0 +1,2 @@
+#WIP this is likly not gona be done most might be code from other scripts but ima put it here for if it does get done 
+#about a script that will backup files (pick the dirs) to a device/dir and check and make sure the data is good and not corrupt 
