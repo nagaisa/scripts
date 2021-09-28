d30d767696c9e0f2f98bf201ea3bee1efc8bbf70
nagaisa <82787802+nagaisa@users.noreply.github.com>
2021-09-27 23:08:54 -0400

    Update YTplaylist-downloader.sh

--- YTplaylist-downloader.sh
+++ YTplaylist-downloader.sh
@@ -1,4 +1,4 @@
-#wip. this script will download a youtube playlist as audio (best, in m4a format)
+#done/may be made better. this script will download a youtube playlist as audio (best, in m4a format)
 #replace the URL with the url of the playlist if you want to save some space and/or dont care you the previw/tubnail remove the embed-thumbnail option
 
 youtube-dl "URL" -v -f "best audio[ext=m4a]" --embed-thumbnail -o ~/Music/"%(title)s.%(ext)s" --playlist-reverse --add-metadata -i
